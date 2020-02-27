90cb7dd75b5a5d492789578462f3c4234debb71c
Armandosz <armando.sz117@gmail.com>
2020-02-27 14:45:07 -0600

    Se agrego el horario

--- localStorage/app.js
+++ localStorage/app.js
@@ -11,6 +11,7 @@ let arrayActividades = [];
 
 const CrearItem = (nombre, actividad, costo) => {
   let item = {
+    horario: horario(),
     nombre: nombre,
     actividad: actividad,
     costo: costo
@@ -49,7 +50,7 @@ const PintarDB = () => {
         listaActividadesUI.innerHTML += `<td>1</td><td>${element.nombre}</td><td>${element.actividad}</td><td>${element.costo}</td><td><span class="float-right"><i class="material-icons">delete</i></span></td>`;
       }else{
         //listaActividadesUI.innerHTML += `<td></td><td>${element.nombre}</td><td>${element.actividad}</td><td>${element.costo}</td>`;
-        listaActividadesUI.insertRow(-1).innerHTML += `<td>${arrayActividades.length}</td><td>${element.nombre}</td><td>${element.actividad}</td><td>${element.costo}</td><td><span id="edit-icon" class="float-center"><i class="material-icons">edit</i></span><span class="float-center"><i class="material-icons">delete</i></span></td> `;
+        listaActividadesUI.insertRow(-1).innerHTML += `<td class="text-center">${element.horario}</td><td class="text-center">${element.nombre}</td><td class="text-center">${element.actividad}</td><td class="text-center">${element.costo}</td><td class="text-center"><span id="edit-icon" class="float-center"><i class="material-icons">edit</i></span><span class="float-center"><i class="material-icons">delete</i></span></td> `;
       }
     });
 
@@ -96,8 +97,9 @@ formularioUI.addEventListener('submit', (e) => {
   let nombreUI = document.querySelector('#nombre').value;
   let actividadUI = document.querySelector('#actividad').value;
   let costoUI = document.querySelector('#costo').value;
+  let horarioUI = horario();
     
- console.log(nombreUI+' + ' + actividadUI + ' + ' + costoUI );
+ console.log(horarioUI+' '+nombreUI+' + ' + actividadUI + ' + ' + costoUI );
 
   CrearItem(nombreUI, actividadUI, costoUI); //agrega un item a la funcion
   GuardarDB();
@@ -140,33 +142,54 @@ listaActividadesUI.addEventListener('click', (e) => {
 });
 
 
+/* Horario 
+var f = new Date();
+document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear()); */
+
+function horario(){
+var hoy = new Date();
+var mes = (hoy.getMonth()+1);
+var cero = mes < 9 ? 0+""+mes : mes;
+var fecha = hoy.getDate()+'/'+cero+'/'+hoy.getFullYear();
+
+//var hora = hoy.getHours()+':'+hoy.getMinutes();
+var hora = formatAMPM(new Date);
+//var hora = hoy.getHours()+':'+hoy.getMinutes()+':'+hoy.getSeconds(); 
+
+var horarioActual = fecha+' - '+hora;
+    return horarioActual;
+}
+
+//var age = 17;
+//var mayor = age 18 ? console.log("si") : console.log("no");
+
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
 
 
 
-/* 1st attempt
-      const countries = [
-        {name: 'USA'},
-        {name: 'India'},
-        {name: 'Argentina'},
-        {name: 'Armenia'}
-      ];
+//sconsole.log(sumarArray(arrayActividades)); // Muestra por consola 78v
 
-      const searchInput = document.querySelector('.search-input');
-      const suggestionsPanel = document.querySelector('.suggestions');
 
-      searchInput.addEventListener('keyup', function() {
-        const input = searchInput.value;
-        suggestionsPanel.innerHTML = '';
-        const suggestions = countries.filter(function(country) {
-          return country.name.toLowerCase().startsWith(input);
-        });
-        suggestions.forEach(function(suggested) {
-          const div = document.createElement('div');
-          div.innerHTML = suggested.name;
-          suggestionsPanel.appendChild(div);
-        });
-        if (input === '') {
-          suggestionsPanel.innerHTML = '';  
-        }
-      })
-*/
+
+var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+function sumarArray(array) {
+    var contador = 0;
+  // Retorna la suma de los números presentes en el array "números"
+    array.forEach(function (numero) {
+        contador+=numero;
+    });
+    return contador;
+}
+
+console.log(sumarArray(numeros)); // Muestra por consola 78 
